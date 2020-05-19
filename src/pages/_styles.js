9a4c36aadcb413e99a0650d325cb3039a3336ef2
Klemen Kogovšek <klemen.kogovsek1@gmail.com>
2020-05-20 00:39:17 +0200

    Fixed missing css import

--- src/pages/_styles.js
+++ src/pages/_styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { motion } from "framer-motion";
 // Style from https://codepen.io/JavaScriptJunkie/pen/pPRooV
 export const Button = styled(motion.button)`
